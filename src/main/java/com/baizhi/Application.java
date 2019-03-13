088e7b386cae581b8c2ed29e6f81f3eb08f952a4
vanessa <vanessa>
2019-03-13 11:41:32 +0800

    cmfz_wjm2.0

--- src/main/java/com/baizhi/Application.java
+++ src/main/java/com/baizhi/Application.java
@@ -11,5 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println("123");
     }
 }
